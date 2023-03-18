c865aaaf5f58e55ef3ed56e0cb8b958b06d5b1e4
33LUKSER <100236048+33LUKSER@users.noreply.github.com>
2023-03-18 03:02:44 +0100

    x

--- PERSONATGE.gd
+++ PERSONATGE.gd
@@ -6,7 +6,7 @@ var gravetat := Vector2.DOWN*500
 var direccio = Vector2.ZERO
 var salt := 400
 var punts := 0
-
+var position_de_plataforma = Vector2(300,150)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,16 +26,24 @@ func _process(delta):
 	if Input.is_action_just_pressed("ves_amunt") and is_on_floor():
 		$Label.text = "SALTA"
 		moviment.y = -salt
+		
 	moviment += direccio.normalized() * velocitat
 	moviment += gravetat*delta
 	moviment = move_and_slide(moviment,Vector2.UP)
-
-
+	
+	if direccio.x == 0:
+		$anim.stop()
+		$Sprite.frame = 0
+	elif direccio.x != 0:
+		if $anim.is_playing() == false:
+			$anim.play("movpersonatge")
 func _punt_tocat(body):
 	punts += 1
 	$Label2.text = "Count: %d" % [punts]
 
 
-func _on_count_renamed():
-	pass # Replace with function body.
+		
 
+
+func _on_Area2D_body_exited(body):
+	body.global_position = position_de_plataforma
